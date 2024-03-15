8227a440e2a9974b30f881358f8c44e867ae0e8f
SamuelLucasVieira <samuellucasvieira.melo@gmail.com>
2024-03-15 18:05:10 -0300

    Implementacao dos comentario na exercicio P2.8

--- p2_8Model.ts
+++ p2_8Model.ts
@@ -1,21 +1,21 @@
 class MyArray {
     lista: number[];
-
+// Lista é o elemento que será usado como base para a recursividade.
     constructor(lista: number[]) {
         this.lista = lista;
     }
-
-    reversa(a: number[]): number[] {
-        if (a.length <= 1) {
+    //Reversa refere-se a receber conteúdo de lista, que aplicado ao caso base concatena os valor lista posição inicial e incrementa na váriavel 
+    reversa(lista: number[]): number[] {
+        if (lista.length <= 1) {
             console.log("Caso Base Atingido");
-            return a;
+            return lista;
         } else {
             console.log("Chamada recursiva");
             console.log("Invocando a reversa");
-            console.log("Invocando Lista( ", a.slice(1, a.length), ");");
-            let reversa_restante = this.reversa(a.slice(1, a.length));
+            console.log("Invocando Lista( ", lista.slice(1, lista.length), ");");
+            let reversa_restante = this.reversa(lista.slice(1, lista.length));
             if(reversa_restante.length >= 1){
-                return reversa_restante.concat(a[0]);
+                return reversa_restante.concat(lista[0]);
             }else{
                 return reversa_restante
             } 
